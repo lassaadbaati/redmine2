6be9ed72cc2a0ffa38e139dc09297435370e1842
Koltsov <koltsov@koltsov-virtual-machine.(none)>
2013-05-22 13:29:12 +0400

    Inline Note Images plugin added

---
+++ plugins/redmine_inline_note_images_plugin/init.rb
@@ -0,0 +1,10 @@
+require_dependency 'inline_image_hook/hooks'
+
+Redmine::Plugin.register :redmine_inline_note_images_plugin do
+  name 'Redmine Inline Note Images plugin'
+  author 'AnjLab'
+  description 'Inline note images for Redmine issues'
+  version '0.0.1'
+  url 'https://github.com/anjlab/redmine_inline_note_images_plugin'
+  author_url 'http://anjlab.com'
+end
